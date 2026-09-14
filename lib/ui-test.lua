796f1a82f3db9768049b5c977ecb906b67758ab3
m2zmforever <mstf@mail.bg>
2026-09-14 15:55:00 +0300

    Update ui-test.lua

--- lib/ui-test.lua
+++ lib/ui-test.lua
@@ -265,15 +265,15 @@ function AevryxLib.Main(Name,X,Y)
         Image = "rbxassetid://138902649769382";
         ImageTransparency = 0;
         ScaleType = Enum.ScaleType.Fit;
-        AnchorPoint = Vector2.new(0,1);
-        Position = UDim2.new(0,8,1,35);
+        AnchorPoint = Vector2.new(0.5,1);
+        Position = UDim2.new(0.5,0,1,35);
         Size = UDim2.new(0,BANNER_SIZE,0,BANNER_SIZE);
         ZIndex = 6;
     })
 
     Topbar.Changed:Connect(function(Property)
         if Property == "Position" then
-            BannerImage.Position = UDim2.new(0,8,1,35)
+            BannerImage.Position = UDim2.new(0.5,0,1,35)
         end
     end)
     local Corner = CreateModule.Instance("UICorner",{
