e1e37f8d83b7bd1fc6b72e5eddd57c76ba781161
m2zmforever <mstf@mail.bg>
2026-09-20 01:38:55 +0300

    Update test-ui.lua

--- lib/test-ui.lua
+++ lib/test-ui.lua
@@ -226,7 +226,7 @@ function AevryxLib.Main(Name,X,Y)
         ImageTransparency = 0;
         ScaleType = Enum.ScaleType.Fit;
         AnchorPoint = Vector2.new(0,1);
-        Position = UDim2.new(0,8,1,35);
+        Position = UDim2.new(0,12,1,35);
         Size = UDim2.new(0,BANNER_SIZE,0,BANNER_SIZE);
         ZIndex = 6;
     })
